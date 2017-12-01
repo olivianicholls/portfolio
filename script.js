76050871dbbfcaffd1f1032356993755e50b33f9
Olivia Nicholls <31240787+olivianicholls@users.noreply.github.com>
2017-12-01 12:20:35 +0000

    Add language icon hover function

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
 
-$('h1').fadeIn(1500);
+$('h1').fadeIn(2500);
 $('h3').fadeIn(2800);
 
 
 $('.menu-btn').click(function() {
 $('.navbar').slideToggle();
-
 });
+  
+  
+  $('.icon-wrapper').hover(function() {
+    $(this).find('p').fadeToggle('3000');
+  });
+  
+
+  
 });
